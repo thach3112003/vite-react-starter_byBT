755ed5568dd78206e3ad6af5a7e0e4f95c244187
thach3112003 <thach3112003@gmail.com>
2025-02-19 14:42:15 +0700

    the first time

---
+++ src/layout.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function Layout() {
+
+  return (
+   <div>Hello world!</div>
+  )
+}
+
+export default Layout
